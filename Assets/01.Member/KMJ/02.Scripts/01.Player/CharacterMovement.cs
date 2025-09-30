fb1568f6cabcd4f1c36d7f1e2973e803ef57f685
kminjae121 <157552653+kminjae121@users.noreply.github.com>
2025-09-30 10:57:49 +0900

    EntityStat

---
+++ Assets/01.Member/KMJ/02.Scripts/01.Player/CharacterMovement.cs
@@ -0,0 +1,15 @@
+﻿using _00.CORE._02.Scripts;
+using _01.Member.KMJ._00.Core._01.Entity._05.Interface;
+using UnityEngine;
+
+namespace _01.Member.KMJ._02.Scripts._01.Player
+{
+    public class CharacterMovement : MonoBehaviour, IEntityComponent
+    {
+        [SerializeField] private StatSO moveSpeedStat;
+        public void Initialize(Entity entity)
+        {
+            
+        }
+    }
+}
