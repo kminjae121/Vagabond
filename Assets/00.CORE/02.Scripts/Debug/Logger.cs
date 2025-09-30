c75eced985a7d265149711cfbcbdad25698bd980
jiho08 <57872222k@gmail.com>
2025-09-30 11:29:13 +0900

    feat : logger

---
+++ Assets/00.CORE/02.Scripts/Debug/Logger.cs
@@ -0,0 +1,22 @@
+#define ONLOG // 디버그 끌 땐 주석 처리
+
+namespace Code.Core.Debug
+{
+    public static class Logger
+    {
+#if ONLOG && UNITY_EDITOR
+        public static void Log<T>(T message) => UnityEngine.Debug.Log(message);
+        public static void LogError<T>(T message) => UnityEngine.Debug.LogError(message);
+        public static void LogWarning<T>(T message) => UnityEngine.Debug.LogWarning(message);
+#else
+        [System.Diagnostics.Conditional("_")]
+        public static void Log<T>(T message) { }
+
+        [System.Diagnostics.Conditional("_")]
+        public static void LogError<T>(T message) { }
+
+        [System.Diagnostics.Conditional("_")]
+        public static void LogWarning<T>(T message) { }
+#endif
+    }
+}
