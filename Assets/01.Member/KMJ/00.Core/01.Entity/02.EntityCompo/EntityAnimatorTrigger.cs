a7f2681a0af80c940707b382e14438487540b5ec
kminjae121 <157552653+kminjae121@users.noreply.github.com>
2025-09-30 10:51:27 +0900

    feat : EntiutySystem

---
+++ Assets/01.Member/KMJ/00.Core/01.Entity/02.EntityCompo/EntityAnimatorTrigger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using _01.Member.KMJ._00.Core._01.Entity._05.Interface;
+using UnityEngine;
+
+namespace _01.Member.KMJ._00.Core._01.Entity._02.EntityCompo
+{
+    public class EntityAnimatorTrigger : MonoBehaviour, IEntityComponent
+    {
+        public Action OnAnimationEndTrigger;
+        //public event Action<bool> OnRollingStatusChange;
+        public event Action OnAttackVFXTrigger;
+        public event Action<bool> OnManualRotationTrigger;
+        public event Action OnDamageCastTrigger;
+        public event Action<bool> OnDamageToggleTrigger;
+        public event Action OnCastSkillTrigger;
+        
+        private global::Entity _entity;
+        
+        public void Initialize(global::Entity entity)
+        {
+            _entity = entity;
+        }
+
+        private void AnimationEnd()
+        {
+            OnAnimationEndTrigger?.Invoke();
+        }
+        
+        // private void RollingStart() => OnRollingStatusChange?.Invoke(true);
+        // private void RollingEnd() => OnRollingStatusChange?.Invoke(false);
+        private void PlayAttackVFX() => OnAttackVFXTrigger?.Invoke();
+        private void StartManualRotation() => OnManualRotationTrigger?.Invoke(true);
+        private void StopManualRotation() => OnManualRotationTrigger?.Invoke(false);
+        private void DamageCast() => OnDamageCastTrigger?.Invoke();
+        private void StartDamageCast() => OnDamageToggleTrigger?.Invoke(true);
+        private void StopDamageCast() => OnDamageToggleTrigger?.Invoke(false);
+        private void CastSkill() => OnCastSkillTrigger?.Invoke();
+        
+    }
+}
---
+++ Assets/01.Member/KMJ/02.Scripts/01.Player/Player.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace _01.Member.KMJ._02.Scripts._01.Player
+{
+    public class Player : Entity
+    {
+        protected override void InitializeComponents()
+        {
+            base.InitializeComponents();
+        }
+    }
+}
