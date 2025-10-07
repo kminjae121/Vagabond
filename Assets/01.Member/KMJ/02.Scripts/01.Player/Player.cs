c5286126b0d719e3ed669e181bdf7b186c92f1be
jiho08 <57872222k@gmail.com>
2025-10-07 22:04:35 +0900

    feat : player manager, bt actions.. etc, fix : player

--- Assets/01.Member/KMJ/02.Scripts/01.Player/Player.cs
+++ Assets/01.Member/KMJ/02.Scripts/01.Player/Player.cs
@@ -4,11 +4,12 @@ using _01.Member.KMJ._00.Core._01.Entity._01.EntityState;
 using _01.Member.KMJ._02.Scripts._01.Player.PlayerWeapon;
 using _01.Member.KMJ._02.Scripts._01.Player.SlidingCompo;
 using _01.Member.KMJ._02.Scripts._01.Player.State;
+using GondrLib.Dependencies;
 using UnityEngine;
 
 namespace _01.Member.KMJ._02.Scripts._01.Player
 {
-    public class Player : Entity
+    public class Player : Entity, IDependencyProvider
     {
         [SerializeField] private StateDataSO[] stateDataList;
         
@@ -24,6 +25,9 @@ namespace _01.Member.KMJ._02.Scripts._01.Player
         public PlayerSword swordCompo { get; private set; }
         
         private bool isJumping = true;
+
+        [Provide]
+        public Player GetPlayer() => this;
         
         protected override void Awake()
         {
