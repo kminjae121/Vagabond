4f1baa5570eab8cec28e1b2ae3f3400214a46b6c
jiho08 <57872222k@gmail.com>
2025-10-19 22:35:39 +0900

    feat : damage caster, melee enemy attack compo.. etc

---
+++ Assets/01.Member/PJH/01.Scripts/Combat/WeaponHolder.cs
@@ -0,0 +1,23 @@
+﻿using Code.Interfaces;
+using UnityEngine;
+
+namespace Code.Entities.Combat
+{
+    public class WeaponHolder : MonoBehaviour, IEntityComponent
+    {
+        [SerializeField] private Weapon[] weapons;
+
+        private Entity _entity;
+        
+        public void Initialize(Entity entity)
+        {
+            _entity = entity;
+        }
+
+        public void DropWeapons()
+        {
+            foreach (var weapon in weapons)
+                weapon.Drop();
+        }
+    }
+}
