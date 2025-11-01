1ba975d1ed2bf6a4ec73b7f2cfa1eb5a533cade3
kminjae121 <157552653+kminjae121@users.noreply.github.com>
2025-11-02 02:04:07 +0900

    feat : Player

---
+++ Assets/01.Member/KMJ/02.Scripts/01.Player/Land/LandCompo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Code.Core._02.Sound;
+using Code.Core.GameEvent;
+using GameEvents;
+using UnityEngine;
+
+namespace _01.Member.KMJ._02.Scripts._01.Player.Land
+{
+    public class LandCompo : MonoBehaviour
+    {
+    }
+}
