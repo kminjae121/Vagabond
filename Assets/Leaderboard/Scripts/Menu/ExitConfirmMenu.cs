a6d94c78ba41de319dbf8933a868dff435069bd1
kimdonghyun81222 <157779631+kimdonghyun81222@users.noreply.github.com>
2025-11-01 22:22:27 +0900

    feat : UGS Leaderboard, All Ui

---
+++ Assets/Leaderboard/Scripts/Menu/ExitConfirmMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ExitConfirmMenu : Panel
+{
+    [SerializeField] private Button confirmButton = null;
+    [SerializeField] private Button cancelButton = null;
+    [SerializeField] private TextMeshProUGUI confirmText = null;
+
+    public override void Initialize()
+    {
+        if (IsInitialized)
+        {
+            return;
+        }
+        
+        confirmButton.onClick.AddListener(ConfirmExit);
+        cancelButton.onClick.AddListener(CancelExit);
+
+        if (confirmText != null)
+        {
+            confirmText.text = "정말로 게임을 종료하시겠습니까?";
+        }
+
+        base.Initialize();
+    }
+
+    private void ConfirmExit()
+    {
+        Debug.Log("게임 종료");
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+    }
+
+    private void CancelExit()
+    {
+        Close();
+    }
+}
